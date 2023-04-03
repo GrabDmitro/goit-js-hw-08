d41f432a4173e64900388b15a34df534a9a250b6
GrabDmitro <d_grab2001@ukr.net>
2023-04-03 17:52:00 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
+const list = galleryItems.map(image => {
+  const fragment = document.createElement('li');
+  fragment.classList.add('gallery__item');
+
+  const link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.href = image.original;
+
+  const img = document.createElement('img');
+  img.classList.add('gallery__image');
+  img.src = image.preview;
+  img.alt = image.description;
+  // img.dataset.source = image.original;
+
+  fragment.appendChild(link);
+  link.appendChild(img);
+  return fragment;
+});
+const galleryRef = document.querySelector('.gallery');
 console.log(galleryItems);
+galleryRef.append(...list);
+new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionDelay: 250,
+  captionsData: 'alt',
+});
