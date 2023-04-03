056d8491a03e1edd458921b5870a0e4c8b36d3ca
GrabDmitro <d_grab2001@ukr.net>
2023-04-03 22:38:15 +0300

    fin

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+console.log(localStorage.getItem('number'));
+const player = new Player('vimeo-player');
+const STORAGE_TIME_KEY = 'current_time';
+if (localStorage.getItem(STORAGE_TIME_KEY)) {
+  player.setCurrentTime(localStorage.getItem(STORAGE_TIME_KEY));
+}
+
+player.on(
+  'timeupdate',
+  throttle(e => localStorage.setItem(STORAGE_TIME_KEY, e.seconds)),
+  2000
+);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  name: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+const STORAGE_FEEDBACK_KEY = 'feedback-form-state';
 
+refs.name.value = JSON.parse(localStorage.getItem(STORAGE_FEEDBACK_KEY)).name;
+refs.textarea.value = JSON.parse(
+  localStorage.getItem(STORAGE_FEEDBACK_KEY)
+).message;
+
+refs.name.addEventListener(
+  'input',
+  throttle(e => {
+    localStorage.setItem(
+      STORAGE_FEEDBACK_KEY,
+      JSON.stringify({
+        name: e.target.value,
+        message: refs.textarea.value,
+      })
+    );
+  }),
+  1000
+);
+refs.textarea.addEventListener(
+  'input',
+  throttle(e => {
+    localStorage.setItem(
+      STORAGE_FEEDBACK_KEY,
+      JSON.stringify({
+        name: refs.name.value,
+        message: e.target.value,
+      })
+    );
+  }),
+  1000
+);
